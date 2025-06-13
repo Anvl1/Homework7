742493cba9a9d335ff054c573c7a3272cc35873f
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-06-14 00:40:14 +1000

    Add files via upload

---
+++ Homework7.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+void swap(int& x, int& y) 
+{
+    x = x + y;
+    y = x - y;
+    x = x - y;
+}
+int main(int argc, char** argv) 
+{
+    int a = 5, b = 8;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
